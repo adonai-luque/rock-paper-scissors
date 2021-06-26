26452cf2f15fe87cf2eea2616243988aa6804bb7
adonai-luque <akluqueprieto@gmail.com>
2021-06-26 02:25:45 -0500

    Results page styled and highlight of winner functionality added

--- script.js
+++ script.js
@@ -14,6 +14,10 @@ const resultGuide = {
   }
 }
 const options = ['rock', 'paper', 'scissors'];
+let player;
+let computer;
+let winner;
+let score = (localStorage.score && 0)
 
 // Media queries
 const body = document.querySelector('body');
@@ -24,14 +28,10 @@ const playerBoard = document.querySelector('.player-board');
 const computerBoard = document.querySelector('.computer-board');
 const outcomeBoard = document.querySelector('.outcome-board')
 const outcome = document.querySelector(".outcome");
-let player;
-let computer;
 let playerChoice = document.querySelector('.player');
 let computerChoice = document.querySelector('.computer');
-
 let rulesModal = document.querySelector('.rules-modal');
 let overlay = document.querySelector('.overlay');
-
 
 // Event listeners
 game.addEventListener('click', attempt);
@@ -45,6 +45,7 @@ function attempt(e) {
     showPlayer();
   }
 }
+
 function showPlayer() {
   game.classList.add('inactive');
   playerChoice.innerHTML = document.querySelector(`.${player}`).innerHTML
@@ -53,10 +54,22 @@ function showPlayer() {
   results.classList.remove('inactive');
   computerBoard.addEventListener('click', showComputer)
 }
+
 function randomChoice(options) {
   let index = Math.floor(Math.random() * options.length);
   return options[index];
 }
+
+function result(player, computer) {
+  if (player === computer) return "IT'S A TIE!";
+  return resultGuide[player][computer] ? "YOU WIN!" : "YOU LOSE!";
+}
+
+function whoWon() {
+  if (resultGuide[player][computer] === undefined) return null
+  return resultGuide[player][computer] ? playerChoice : computerChoice;
+}
+
 function showComputer() {
   computerBoard.removeEventListener('click', showComputer)
   computer = randomChoice(options);
@@ -64,30 +77,31 @@ function showComputer() {
   computerChoice.classList.add(computer);
   computerChoice.classList.remove('empty-choice');
   computerChoice.classList.add('large-choice');
-  outcome.textContent = result(player, computer);
-  setTimeout(() => showOutcome(), 500);
+  showOutcome();
 }
-function result(player, computer) {
-  if (player === computer) return "IT'S A TIE";
-  return resultGuide[player][computer] ? "YOU WIN" : "YOU LOSE";
-}
+
 function showOutcome() {
+  winnerChoice =  whoWon()
+  winnerChoice && winnerChoice.classList.add('winner');
+  outcome.textContent = result(player, computer);
   results.removeEventListener('click', showOutcome)
   outcomeBoard.classList.remove('inactive');
 }
+
 function clearResults() {
   outcomeBoard.classList.add('inactive');
   playerChoice.innerHTML = "";
   playerChoice.classList.remove(player);
   playerChoice.classList.remove('large-choice');
-  computerChoice.innerHTML = "";
+  computerChoice.innerHTML = "\n        <h3>CLICK TO REVEAL</h3>\n      ";
   computerChoice.classList.remove(computer);
   computerChoice.classList.add('empty-choice');
   computerChoice.classList.remove('large-choice');
   outcome.textContent = ""
+  winnerChoice && winnerChoice.classList.remove('winner');
 }
+
 function runGame() {
-  console.log('runGame');
   clearResults();
   game.classList.remove('inactive');
   results.classList.add('inactive');
